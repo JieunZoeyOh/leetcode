17a9edd83518006129810ceba15a01c5982959d4
Jieun Oh <67398484+JieunZoeyOh@users.noreply.github.com>
2024-09-21 23:58:57 +0900

    Time: 80 ms (19.77%), Space: 58.6 MB (19.69%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -5,13 +5,13 @@
 const maxSubArray = (nums) => {
   if (nums.length === 0) return 0;
 
-  let maxSum = nums[0];
-  let tempSum = nums[0];
+  let max = nums[0];
+  let acc = nums[0];
 
   for (let i = 1; i < nums.length; i++) {
-    tempSum = Math.max(tempSum + nums[i], nums[i]);
-    maxSum = Math.max(maxSum, tempSum);
+    acc = Math.max(acc + nums[i], nums[i]);
+    max = Math.max(max, acc);
   }
 
-  return maxSum;
+  return max;
 };
