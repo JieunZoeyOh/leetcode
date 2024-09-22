5af9a836e33d956d6a884d2ae54180d08c65346a
Jieun Oh <67398484+JieunZoeyOh@users.noreply.github.com>
2024-09-22 15:17:27 +0900

    Time: 109 ms (21.03%), Space: 61.5 MB (5.7%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+const merge = (intervals) => {
+  if (intervals.length === 1) return intervals;
+
+  intervals.sort(([s1], [s2]) => s1 - s2);
+  
+  const result = [];
+  const acc = [...intervals[0]];
+
+  for (let i = 1; i < intervals.length; i++) {
+    const [start, end] = intervals[i];
+
+    if (end < acc[0] || acc[1] < start) {
+      result.push([...acc]);
+      acc[0] = start;
+      acc[1] = end;
+    } else {
+      acc[0] = Math.min(acc[0], start);
+      acc[1] = Math.max(acc[1], end);
+    }
+  }
+
+  result.push([...acc]);
+
+  return result;
+};
