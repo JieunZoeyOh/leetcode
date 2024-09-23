e137288803627f37b0a3bccd6d06605458d09e8f
Jieun Oh <67398484+JieunZoeyOh@users.noreply.github.com>
2024-09-23 20:37:44 +0900

    Time: 73 ms (44.73%), Space: 58.5 MB (89%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.js
@@ -15,11 +15,9 @@
 const lowestCommonAncestor = (root, p, q) => {
   if (p.val < root.val && q.val < root.val) {
     return lowestCommonAncestor(root.left, p, q);
-  }
-  
-  if (p.val > root.val && q.val > root.val) {
+  } else if (p.val > root.val && q.val > root.val) {
     return lowestCommonAncestor(root.right, p, q);
+  } else {
+    return root;
   }
-
-  return root;
 };
