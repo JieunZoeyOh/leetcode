f7af16978865bae7a72e0ac7d4bc90ecdcca5c2a
Jieun Oh <67398484+JieunZoeyOh@users.noreply.github.com>
2024-10-02 22:46:58 +0900

    Time: 162 ms (47.02%), Space: 60.7 MB (45.65%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.js
+++ 0542-01-matrix/0542-01-matrix.js
@@ -3,14 +3,13 @@
  * @return {number[][]}
  */
 const updateMatrix = (mat) => {
-  const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-  const result = Array.from({ length: mat.length }, () => new Array(mat[0].length).fill(null));
+  const d = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+  const result = Array.from({ length: mat.length }, () => Array(mat[0].length).fill(0));
   const memory = [];
-  
+
   for (let i = 0; i < mat.length; i++) {
     for (let j = 0; j < mat[i].length; j++) {
       if (mat[i][j] === 0) {
-        result[i][j] = 0;
         memory.push([i, j]);
       } else {
         result[i][j] = Infinity;
@@ -19,17 +18,17 @@ const updateMatrix = (mat) => {
   }
 
   while (memory.length > 0) {
-    const [row, column] = memory.shift();
-    const count = result[row][column];
+    const [x, y] = memory.shift();
+    const count = result[x][y];
 
-    for (const [dr, dc] of directions) {
-      const newRow = row + dr;
-      const newColumn = column + dc;
+    for (let i = 0; i < d.length; i++) {
+      const [dx, dy] = d[i];
+      const [newX, newY] = [dx + x, dy + y];
 
-      if (0 <= newRow && newRow < result.length && 0 <= newColumn && newColumn < result[0].length) {
-        if (count + 1 < result[newRow][newColumn]) {
-          result[newRow][newColumn] = count + 1;
-          memory.push([newRow, newColumn]);
+      if (0 <= newX && newX < mat.length && 0 <= newY && newY < mat[0].length) {
+        if (count + 1 < result[newX][newY]) {
+          result[newX][newY] = count + 1;
+          memory.push([newX, newY]);
         }
       }
     }
