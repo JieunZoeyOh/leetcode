f6577facbdc5740d8851979b7d42a7c7ad9d19ee
Jieun Oh <67398484+JieunZoeyOh@users.noreply.github.com>
2025-05-18 15:17:24 +0900

    Time: 0 ms (100%), Space: 58.5 MB (6.74%) - LeetHub

--- 0792-binary-search/0792-binary-search.js
+++ 0792-binary-search/0792-binary-search.js
@@ -7,14 +7,14 @@ const search = (nums, target) => {
   let left = 0;
   let right = nums.length - 1;
 
-  while (left <= right) {
-    const mid = Math.ceil((left + right) / 2);
-    if (nums[mid] === target) return mid;
-
-    if (nums[mid] < target) {
-      left = mid + 1;
-    } else {
+  while(left <= right) {
+    const mid = Math.floor((left + right) / 2);
+    if (target === nums[mid]){
+      return mid;
+    } else if (target < nums[mid]) {
       right = mid - 1;
+    } else {
+      left = mid + 1;
     }
   }
 
