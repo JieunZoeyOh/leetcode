b55b3bc18b1742a648677defda548c9b77befd3c
Jieun Oh <67398484+JieunZoeyOh@users.noreply.github.com>
2025-05-20 11:57:12 +0900

    Time: 29 ms (5.24%), Space: 64 MB (33.51%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,15 +3,34 @@
  * @return {number}
  */
 const maxSubArray = (nums) => {
-  if (nums.length === 0) return 0;
+  return getMaxSum(nums, 0, nums.length - 1);
+};
 
-  let max = nums[0];
-  let acc = nums[0];
+const getMaxSum = (nums, left, right) => {
+  if (left === right) return nums[left];
 
-  for (let i = 1; i < nums.length; i++) {
-    acc = Math.max(acc + nums[i], nums[i]);
-    max = Math.max(max, acc);
+  const mid = Math.floor((left + right) / 2);
+  const leftMaxSum = getMaxSum(nums, left, mid);
+  const rightMaxSum = getMaxSum(nums, mid + 1, right);
+  const centerMaxSum = getCenterSum(nums, left, right, mid);
+
+  return Math.max(leftMaxSum, rightMaxSum, centerMaxSum);
+};
+
+const getCenterSum = (nums, left, right, mid) => {
+  let leftAcc = 0;
+  let leftMaxSum = -Infinity;
+  for (let i = mid; i >= left; i--) {
+    leftAcc += nums[i];
+    leftMaxSum = Math.max(leftMaxSum, leftAcc);
   }
 
-  return max;
+  let rightAcc = 0;
+  let rightMaxSum = -Infinity;
+  for (let i = mid + 1; i <= right; i++) {
+    rightAcc += nums[i];
+    rightMaxSum = Math.max(rightMaxSum, rightAcc);
+  }
+
+  return leftMaxSum + rightMaxSum;
 };
