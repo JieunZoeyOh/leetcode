cb450f204b672c6173e6fff63a571b34cf684527
Jieun Oh <67398484+JieunZoeyOh@users.noreply.github.com>
2025-05-22 20:07:38 +0900

    Time: 6 ms (21.25%), Space: 59 MB (41.29%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.js
+++ 0057-insert-interval/0057-insert-interval.js
@@ -4,24 +4,24 @@
  * @return {number[][]}
  */
 const insert = (intervals, newInterval) => {
+  const listSize = intervals.length;
   const result = [];
-  const len = intervals.length;
   let i = 0;
-
-  while (i < len && intervals[i][1] < newInterval[0]) {
+  
+  while (i < listSize && intervals[i][1] < newInterval[0]) {
     result.push(intervals[i]);
     i++;
   }
 
-  while (i < len && intervals[i][0] <= newInterval[1]) {
-    newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
-    newInterval[1] = Math.max(newInterval[1], intervals[i][1]);
+  while(i < listSize && intervals[i][0] <= newInterval[1]) {
+    newInterval[0] = Math.min(intervals[i][0], newInterval[0]);
+    newInterval[1] = Math.max(intervals[i][1], newInterval[1]);
     i++;
   }
 
   result.push(newInterval);
 
-  while (i < len) {
+  while (i < listSize) {
     result.push(intervals[i]);
     i++;
   }
