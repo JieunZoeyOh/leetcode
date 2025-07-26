ae325ccbb04c9b9b65da7e41d0aacf6db063bfac
Jieun Oh <67398484+JieunZoeyOh@users.noreply.github.com>
2025-07-26 17:33:03 +0900

    Time: 47 ms (43.5%), Space: 80.5 MB (18.06%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.js
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.js
@@ -1,20 +1,29 @@
-const Trie = function() {
-  this.root = {};
+class TrieNode {
+  constructor() {
+    this.children = new Map();
+    this.isEnd = false;
+  }
+}
+
+var Trie = function() {
+  this.root = new TrieNode();
 };
 
-/**
+/** 
  * @param {string} word
  * @return {void}
  */
 Trie.prototype.insert = function(word) {
-  let node = this.root;
+  let currentNode = this.root;
 
   for (const char of word) {
-    if (!node[char]) node[char] = {};
-    node = node[char];
+    if (!currentNode.children.has(char)) {
+      currentNode.children.set(char, new TrieNode());
+    }
+    currentNode = currentNode.children.get(char);
   }
 
-  node.isEnd = true;
+  currentNode.isEnd = true;
 };
 
 /** 
@@ -22,8 +31,16 @@ Trie.prototype.insert = function(word) {
  * @return {boolean}
  */
 Trie.prototype.search = function(word) {
-  const node = this.traverse(word);
-  return !!node?.isEnd;
+  let currentNode = this.root;
+
+  for (const char of word) {
+    if (!currentNode.children.has(char)) {
+      return false;
+    }
+    currentNode = currentNode.children.get(char);
+  }
+
+  return currentNode.isEnd;
 };
 
 /** 
@@ -31,20 +48,17 @@ Trie.prototype.search = function(word) {
  * @return {boolean}
  */
 Trie.prototype.startsWith = function(prefix) {
-  const node = this.traverse(prefix);
-  return !!node;
-};
+    let currentNode = this.root;
 
-Trie.prototype.traverse = function(word) {
-  let node = this.root;
-
-  for (const char of word) {
-    if (!node[char]) return null;
-    node = node[char];
+  for (const char of prefix) {
+    if (!currentNode.children.has(char)) {
+      return false;
+    }
+    currentNode = currentNode.children.get(char);
   }
 
-  return node;
-}
+  return true;
+};
 
 /** 
  * Your Trie object will be instantiated and called as such:
@@ -53,3 +67,4 @@ Trie.prototype.traverse = function(word) {
  * var param_2 = obj.search(word)
  * var param_3 = obj.startsWith(prefix)
  */
+ 
