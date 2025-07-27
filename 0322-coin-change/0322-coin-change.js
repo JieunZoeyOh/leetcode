144488ef2e5a85ce1cc6074906a7ca4f13c8ef3f
Jieun Oh <67398484+JieunZoeyOh@users.noreply.github.com>
2025-07-27 12:47:45 +0900

    Time: 35 ms (68.4%), Space: 57.5 MB (96.17%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -3,17 +3,22 @@
  * @param {number} amount
  * @return {number}
  */
-const coinChange = function(coins, amount) {
+const coinChange = (coins, amount) => {
   const dp = new Array(amount + 1).fill(Infinity);
+
   dp[0] = 0;
 
-  for (let i = 0; i <= amount; i++) {
+  for (let i = 0; i < dp.length; i++) {
     for (const coin of coins) {
-      if (coin > i) continue;
-      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+      if (coin <= i) {
+        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+      }
     }
   }
 
-  const minCount = dp[amount];
-  return minCount === Infinity ? -1 : minCount;
+  if (dp[amount] === Infinity) {
+    return -1;
+  }
+
+  return dp[amount];
 };
