d643fd2d058b2d3cf243e21579a3ffa9917469a8
Jieun Oh <67398484+JieunZoeyOh@users.noreply.github.com>
2025-07-29 16:30:36 +0900

    Time: 6 ms (67.82%), Space: 69.3 MB (75.56%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.js
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.js
@@ -2,20 +2,20 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const productExceptSelf = function(nums) {
-  const result = new Array(nums.length).fill(1);
+const productExceptSelf = (nums) => {
+  const answer = new Array(nums.length).fill(1);
 
-  let before = 1;
-  for (let i = 1; i < nums.length; i++) {
-    before *= nums[i - 1];
-    result[i] = before;
+  let leftProduct = 1;
+  for (let i = 0; i < nums.length; i++) {
+    answer[i] = leftProduct;
+    leftProduct *= nums[i];
   }
 
-  let after = 1;
-  for (let i = nums.length - 2; i >= 0; i--) {
-    after *= nums[i + 1];
-    result[i] *= after;
+  let rightProduct = 1;
+  for (let i = nums.length - 1; i >= 0; i--) {
+    answer[i] *= rightProduct;
+    rightProduct *= nums[i];
   }
 
-  return result;
+  return answer;
 };
