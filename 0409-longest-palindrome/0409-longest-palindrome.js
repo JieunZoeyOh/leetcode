01c36bf2fbdefc5c07717979defebe29aa81e6e3
Jieun Oh <67398484+JieunZoeyOh@users.noreply.github.com>
2025-07-31 11:12:01 +0900

    Time: 4 ms (74.6%), Space: 56.1 MB (30.87%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.js
+++ 0409-longest-palindrome/0409-longest-palindrome.js
@@ -5,23 +5,15 @@
 const longestPalindrome = (s) => {
   if (s.length === 1) return 1;
 
-  const map = new Map();
+  const set = new Set();
 
   for (const char of s) {
-    map.set(char, (map.get(char) ?? 0) + 1);
-  }
-
-  let result = 0;
-  let extra = 0;
-
-  for (const count of map.values()) {
-    if (count % 2 === 1) {
-      result += count - 1;
-      extra = 1;
+    if (set.has(char)) {
+      set.delete(char);
     } else {
-      result += count;
+      set.add(char);
     }
   }
 
-  return result + extra;
+  return s.length - set.size + (set.size > 0 ? 1 : 0);
 };
