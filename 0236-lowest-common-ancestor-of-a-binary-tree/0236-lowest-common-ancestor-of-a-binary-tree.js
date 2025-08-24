21aeba36060b267c5c612046a6c2de0c603cceea
Jieun Oh <67398484+JieunZoeyOh@users.noreply.github.com>
2025-08-24 16:07:04 +0900

    Time: 61 ms (37.59%), Space: 65.2 MB (36.28%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.js
@@ -12,14 +12,16 @@
  * @return {TreeNode}
  */
 const lowestCommonAncestor = (root, p, q) => {
-  if (root === null) return null;
-
-  if (root === p || root === q) return root;
+  if (!root || root === p || root === q) {
+    return root;
+  }
 
   const left = lowestCommonAncestor(root.left, p, q);
   const right = lowestCommonAncestor(root.right, p, q);
 
-  if (left && right) return root;
+  if (left && right) {
+    return root;
+  }
 
   return left || right;
 };
